9e8bb95026f49b0d9d3aa3cb8603593066544d29
arjunuvladimer <arjunuvladimer@gmail.com>
2020-12-10 22:23:21 +0530

    Folder Structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import App1 from './App1'
+
+function App() {
+  return (
+    // JSX Elements
+    <div>
+      <h1>Header 1</h1>
+      <p> Some Paragraph</p>
+      
+    </div>
+  );
+}
+
+
+
+export default App
---
+++ src/App1.js
@@ -0,0 +1,10 @@
+function App1(){
+    return (
+        <div>
+            <p> Some Text</p>
+            <h2>Some Header 2</h2>
+        </div>
+    )
+}
+
+export default App1
